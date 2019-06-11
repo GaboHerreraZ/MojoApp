ec57909879e9575aa316e87bcf8c2abdda0e3ccc
Gabriel <herzar_620@hotmail.com>
2019-06-11 15:25:40 -0500

    Se agregan filtro a vista de ingresos

--- MojoApp/src/app/app.component.ts
+++ MojoApp/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ComunesService } from './servicios/mojo/comunes/comunes.service';
-
+import {NgSelectModule, NgOption} from '@ng-select/ng-select';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -21,6 +21,14 @@ export class AppComponent implements OnInit{
       console.log("Prueba del servicios",res);
     });
   }
+
+  cities = [
+    {id: 1, name: 'Vilnius'},
+    {id: 2, name: 'Kaunas'},
+    {id: 3, name: 'Pavilnys'},
+    {id: 4, name: 'Pabradė'},
+    {id: 5, name: 'Klaipėda'}
+];
   
   
 }
